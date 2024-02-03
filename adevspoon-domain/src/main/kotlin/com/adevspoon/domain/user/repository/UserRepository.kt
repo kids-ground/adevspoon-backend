60ae339aae697b432feb3095c249f30eabf493ec
RokwonK <wonrok97@naver.com>
2024-02-03 20:23:59 +0900

    🎨 local 테스트 환경설정 변경

--- adevspoon-domain/src/main/kotlin/com/adevspoon/domain/user/repository/UserRepository.kt
+++ adevspoon-domain/src/main/kotlin/com/adevspoon/domain/user/repository/UserRepository.kt
@@ -1,4 +1,4 @@
-package com.adevspoon.domain.user.domain.repository
+package com.adevspoon.domain.user.repository
 
 import com.adevspoon.domain.user.domain.User
 import org.springframework.data.jpa.repository.JpaRepository
