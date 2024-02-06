114c5726161f88c1b6802df2a4365db0e5189d21
RokwonK <wonrok97@naver.com>
2024-02-06 17:34:58 +0900

    ✨ common 모듈 내 jwt 의존성 및 프로퍼티(local) 정리

--- adevspoon-common/build.gradle.kts
+++ adevspoon-common/build.gradle.kts
@@ -6,3 +6,8 @@ plugins {
     id("com.adevspoon.kotlin-common-conventions")
 }
 
+dependencies {
+    implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
+}
