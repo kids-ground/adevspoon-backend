fd2a5ae30932a1f47335a8b67f93b4740d10c19d
RokwonK <wonrok97@naver.com>
2024-02-08 18:52:12 +0900

    [feat/1] ✨ 트랜잭션 의존성 추가

--- adevspoon-api/build.gradle.kts
+++ adevspoon-api/build.gradle.kts
@@ -17,8 +17,11 @@ dependencies {
     implementation(project(":adevspoon-infrastructure"))
 
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework:spring-tx")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
     implementation("org.springframework.boot:spring-boot-starter-security")
+
 //    developmentOnly("org.springframework.boot:spring-boot-docker-compose")
 
     testImplementation("org.springframework.security:spring-security-test")
