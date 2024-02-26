415dfddf998e60278cb8a72b082ffae63d8f16d0
RokwonK <wonrok97@naver.com>
2024-02-26 21:08:44 +0900

    [feat/1] ✨ OAuthAdapterImpl 구현 완료

--- adevspoon-infrastructure/src/main/kotlin/com/adevspoon/infrastructure/oauth/service/OAuthAdapterImpl.kt
+++ adevspoon-infrastructure/src/main/kotlin/com/adevspoon/infrastructure/oauth/service/OAuthAdapterImpl.kt
@@ -2,6 +2,7 @@ package com.adevspoon.infrastructure.oauth.service
 
 import com.adevspoon.infrastructure.oauth.client.AppleFeignClient
 import com.adevspoon.infrastructure.oauth.client.KakaoFeignClient
+import com.adevspoon.infrastructure.oauth.dto.OAuthType
 import com.adevspoon.infrastructure.oauth.dto.OAuthUserInfoRequest
 import com.adevspoon.infrastructure.oauth.dto.OAuthUserInfoResponse
 import org.springframework.stereotype.Component
@@ -9,9 +10,27 @@ import org.springframework.stereotype.Component
 @Component
 class OAuthAdapterImpl(
     private val kakaoFeignClient: KakaoFeignClient,
-    private val appleFeignClient: AppleFeignClient,
+    private val appleKeyService: AppleKeyService,
 ): OAuthAdapter {
-    override fun getOAuthUserInfo(oAuthUserInfoRequest: OAuthUserInfoRequest): OAuthUserInfoResponse {
-        TODO("Not yet implemented")
-    }
+    override fun getOAuthUserInfo(oAuthUserInfoRequest: OAuthUserInfoRequest): OAuthUserInfoResponse =
+        when (oAuthUserInfoRequest.type) {
+            OAuthType.KAKAO -> getKakaoUserInfo(oAuthUserInfoRequest.kakaoAccessToken)
+            OAuthType.APPLE -> getAppleUserInfo(oAuthUserInfoRequest.appleIdentityToken)
+        }
+
+    private fun getKakaoUserInfo(accessToken: String?): OAuthUserInfoResponse =
+        accessToken?.let {
+            OAuthUserInfoResponse(
+                id = "${kakaoFeignClient.getUserProfile("Bearer $accessToken").id}",
+                type = OAuthType.KAKAO
+            )
+        } ?: throw IllegalArgumentException("accessToken is null")
+
+    private fun getAppleUserInfo(identityToken: String?): OAuthUserInfoResponse =
+        identityToken?.let {
+            OAuthUserInfoResponse(
+                id = appleKeyService.getIdentityKey(it),
+                type = OAuthType.APPLE
+            )
+        } ?: throw IllegalArgumentException("identityToken is null")
 }
