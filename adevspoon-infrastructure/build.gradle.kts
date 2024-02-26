261c0f422ab45246648272444d229f6e1f020b00
RokwonK <wonrok97@naver.com>
2024-02-26 21:08:17 +0900

    [feat/1] ✨ infra 모듈 내 jwt 의존성 추가

--- adevspoon-infrastructure/build.gradle.kts
+++ adevspoon-infrastructure/build.gradle.kts
@@ -20,5 +20,9 @@ dependencyManagement {
 
 dependencies {
     implementation("org.springframework.cloud:spring-cloud-starter-openfeign:4.0.3")
+    implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
+
 }
 
