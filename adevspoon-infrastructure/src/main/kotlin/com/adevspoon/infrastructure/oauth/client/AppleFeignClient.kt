7061426b3952996b6f962a4b48d6abc774ce875a
RokwonK <wonrok97@naver.com>
2024-02-26 21:05:33 +0900

    [feat/1] 🎨 AppleFeignClient 메서드명 수정

--- adevspoon-infrastructure/src/main/kotlin/com/adevspoon/infrastructure/oauth/client/AppleFeignClient.kt
+++ adevspoon-infrastructure/src/main/kotlin/com/adevspoon/infrastructure/oauth/client/AppleFeignClient.kt
@@ -8,5 +8,5 @@ import org.springframework.web.bind.annotation.GetMapping
 @FeignClient(name = "apple", url = "\${feign.apple.url}")
 interface AppleFeignClient {
     @GetMapping("/auth/keys")
-    fun authKeys(): AppleAuthKeysResponse
+    fun getAuthKeys(): AppleAuthKeysResponse
 }
