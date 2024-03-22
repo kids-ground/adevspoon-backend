245034b90ce05b9337a0482b7525b60d526a84e1
RokwonK <wonrok97@naver.com>
2024-03-22 21:37:48 +0900

    [feat/#11] ✨ Request Param 내 들어오는 LegacyDtoEnum 변환용 ConverterFactory 정의 및 등록

---
+++ adevspoon-api/src/main/kotlin/com/adevspoon/api/config/controller/converter/StringToLegacyDtoEnumConverterFactory.kt
@@ -0,0 +1,18 @@
+package com.adevspoon.api.config.controller.converter
+
+import com.adevspoon.api.common.dto.LegacyDtoEnum
+import org.springframework.core.convert.converter.Converter
+import org.springframework.core.convert.converter.ConverterFactory
+
+class StringToLegacyDtoEnumConverterFactory<F>: ConverterFactory<String, F> where F: Enum<*>, F: LegacyDtoEnum {
+    override fun <T : F> getConverter(targetType: Class<T>): Converter<String, T> {
+        return StringToEnumConverter(targetType)
+    }
+
+    private class StringToEnumConverter<T : Enum<*>?>(private val enumType: Class<T>) : Converter<String, T> {
+        override fun convert(source: String): T {
+            val type = enumType as Class<out Enum<*>>
+            return java.lang.Enum.valueOf(type, source.uppercase()) as T
+        }
+    }
+}
--- adevspoon-api/src/main/kotlin/com/adevspoon/api/dummy/DummyController.kt
+++ adevspoon-api/src/main/kotlin/com/adevspoon/api/dummy/DummyController.kt
@@ -2,12 +2,15 @@ package com.adevspoon.api.dummy
 
 import com.adevspoon.api.common.annotation.RequestUser
 import com.adevspoon.api.common.annotation.SecurityIgnored
+import com.adevspoon.api.common.dto.LegacyDtoEnum
 import com.adevspoon.api.common.dto.RequestUserInfo
 import com.adevspoon.api.config.swagger.SWAGGER_TAG_EXAMPLE
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -17,12 +20,20 @@ import org.springframework.web.bind.annotation.RestController
 class DummyController{
     private val log = LoggerFactory.getLogger(this.javaClass)!!
 
-    @Operation(summary = "non-security 테스트용", description = "Security 필요없음")
+    @Operation(summary = "non-security GET 테스트용", description = "Security 필요없음")
     @GetMapping
     @SecurityIgnored
-    fun dummyTest(param: DummyParam): String {
+    fun dummyTest(param: DummyQueryRequest): DummyResponse {
         log.info("dummy ㅎ $param")
-        return "dummy ㅎ2"
+        return DummyResponse("dummy", DummyEnum.DUMMY1)
+    }
+
+    @Operation(summary = "non-security POST 테스트용", description = "Security 필요없음")
+    @PostMapping
+    @SecurityIgnored
+    fun dummyPostTest(@RequestBody request: DummyRequest): DummyResponse {
+        log.info("dummy ㅎ $request")
+        return DummyResponse("dummy", DummyEnum.DUMMY1)
     }
 
     @Operation(summary = "security 테스트용", description = "Security 필요함")
@@ -33,11 +44,21 @@ class DummyController{
 }
 
 
-class DummyParam{
-    var name: String? = null
-    var age: Int? = null
+data class DummyRequest(
+    var name: String? = null,
+    var enum: DummyEnum? = null,
+)
 
-    override fun toString(): String{
-        return "name: $name, age: $age"
-    }
+data class DummyQueryRequest(
+    var name: String? = null,
+    var enum: DummyEnum? = null,
+)
+
+data class DummyResponse(
+    var name: String,
+    var enum: DummyEnum
+)
+
+enum class DummyEnum: LegacyDtoEnum {
+    DUMMY1, DUMMY2, DUMMY3
 }
