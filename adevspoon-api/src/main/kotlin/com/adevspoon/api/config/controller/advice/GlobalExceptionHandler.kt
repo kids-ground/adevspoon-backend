716b107c3689340aa4aaaedf3670b34d5c450f3e
RokwonK <wonrok97@naver.com>
2024-03-25 17:46:04 +0900

    [feat/#11] ✨ HttpeMessageNotReadable 에외 커스텀

--- adevspoon-api/src/main/kotlin/com/adevspoon/api/config/controller/advice/GlobalExceptionHandler.kt
+++ adevspoon-api/src/main/kotlin/com/adevspoon/api/config/controller/advice/GlobalExceptionHandler.kt
@@ -1,37 +1,26 @@
 package com.adevspoon.api.config.controller.advice
 
+
 import com.adevspoon.common.dto.ErrorResponse
 import com.adevspoon.common.exception.AdevspoonException
 import com.adevspoon.common.exception.CommonErrorCode
 import com.adevspoon.common.exception.ExternalException
+import com.fasterxml.jackson.databind.exc.MismatchedInputException
 import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
 import org.springframework.http.HttpStatusCode
 import org.springframework.http.ResponseEntity
+import org.springframework.http.converter.HttpMessageNotReadableException
 import org.springframework.lang.Nullable
 import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 import org.springframework.web.context.request.WebRequest
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
-import java.net.HttpURLConnection.HTTP_BAD_REQUEST
 
 
 @RestControllerAdvice
 class GlobalExceptionHandler: ResponseEntityExceptionHandler() {
-
-    // MVC 처리 예외 래핑처리
-    override fun handleExceptionInternal(
-        ex: java.lang.Exception,
-        @Nullable body: Any?,
-        headers: HttpHeaders,
-        statusCode: HttpStatusCode,
-        request: WebRequest
-    ): ResponseEntity<Any>? {
-        return ResponseEntity
-            .status(statusCode.value())
-            .body(ErrorResponse(statusCode.value(), ex.message ?: ""))
-    }
-
     // Validation 예외 처리
     override fun handleMethodArgumentNotValid(
         ex: MethodArgumentNotValidException,
@@ -39,27 +28,61 @@ class GlobalExceptionHandler: ResponseEntityExceptionHandler() {
         status: HttpStatusCode,
         request: WebRequest
     ): ResponseEntity<Any>? {
+        val errorMessage = ex.bindingResult.allErrors.map { it.defaultMessage }.joinToString { "\n" }
+        logger.warn("Validation 실패 : $errorMessage")
         return ResponseEntity
-            .status(HTTP_BAD_REQUEST)
+            .status(HttpStatus.BAD_REQUEST)
             .body(ErrorResponse(
                 CommonErrorCode.BAD_REQUEST.getErrorInfo().code,
                 ex.bindingResult.allErrors.map { it.defaultMessage }.joinToString { "\n" }
             ))
     }
 
-    // 앱 내 RuntimeException 처리
+    override fun handleHttpMessageNotReadable(
+        ex: HttpMessageNotReadableException,
+        headers: HttpHeaders,
+        status: HttpStatusCode,
+        request: WebRequest
+    ): ResponseEntity<Any>? {
+        val errorInfo = when (val cause = ex.rootCause) {
+            is AdevspoonException -> cause.errorInfo
+            is MismatchedInputException -> CommonErrorCode.BAD_REQUEST
+                .getErrorInfo()
+                .also { it.message = "유효하지 않은 값입니다: ${cause.path.map { it.fieldName }.joinToString(".")}" }
+            else -> CommonErrorCode.BAD_REQUEST.getErrorInfo()
+        }
+        logger.warn("잘못된 요청 타입매핑 : ${errorInfo.message}")
+        return ResponseEntity
+            .status(errorInfo.status)
+            .body(ErrorResponse(errorInfo.code, errorInfo.message))
+    }
+
+    override fun handleExceptionInternal(
+        ex: java.lang.Exception,
+        @Nullable body: Any?,
+        headers: HttpHeaders,
+        statusCode: HttpStatusCode,
+        request: WebRequest
+    ): ResponseEntity<Any>? {
+        logger.warn("Web MVC 예외 : ${ex.message}")
+        return ResponseEntity
+            .status(statusCode.value())
+            .body(ErrorResponse(statusCode.value(), ex.message ?: ""))
+    }
+
     @ExceptionHandler(AdevspoonException::class)
     fun handleAdevspoonException(e: AdevspoonException): ResponseEntity<ErrorResponse> {
+        logger.warn("인 앱 예외 발생 : ${e.errorInfo.message}")
         val errorInfo = e.errorInfo
         return ResponseEntity
             .status(errorInfo.status)
             .body(ErrorResponse(errorInfo.code, errorInfo.message))
     }
 
-    // 외부시스템 사용시 예외처리
     @ExceptionHandler(ExternalException::class)
     fun handleExternalException(e: ExternalException): ResponseEntity<ErrorResponse> {
         logger.error("""
+            [외부 시스템 예외]
             message: ${e.message}
             reason: ${e.reason}
         """.trimIndent())
@@ -70,10 +93,10 @@ class GlobalExceptionHandler: ResponseEntityExceptionHandler() {
     }
 
     // TODO: - ErrorResponse 내 TraceID를 넘기는 것도 고려해보기
-    // 처리하지 못한 에러 처리
     @ExceptionHandler(Exception::class)
     fun handleUncaughtException(e: Exception): ResponseEntity<ErrorResponse> {
         logger.error("""
+            [에러]
             message: ${e.message}
             type: ${e.javaClass.simpleName}
         """.trimIndent())
