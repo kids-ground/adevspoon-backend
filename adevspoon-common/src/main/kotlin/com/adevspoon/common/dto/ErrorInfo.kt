9a492ff616f7ede8e20772143ec8646e5af77a3f
RokwonK <wonrok97@naver.com>
2024-03-25 17:43:07 +0900

    [feat/#11] 🎨 AdevspoonException 및 ErrorInfo 내 message custon 가능하도록 개방

--- adevspoon-common/src/main/kotlin/com/adevspoon/common/dto/ErrorInfo.kt
+++ adevspoon-common/src/main/kotlin/com/adevspoon/common/dto/ErrorInfo.kt
@@ -3,5 +3,5 @@ package com.adevspoon.common.dto
 data class ErrorInfo(
     val status: Int, // HTTP status code
     val code: Int, // error code
-    val message: String, // error message
+    var message: String, // error message
 )
--- adevspoon-common/src/main/kotlin/com/adevspoon/common/exception/AdevspoonException.kt
+++ adevspoon-common/src/main/kotlin/com/adevspoon/common/exception/AdevspoonException.kt
@@ -1,8 +1,15 @@
 package com.adevspoon.common.exception
 
+import com.adevspoon.common.dto.ErrorInfo
+
 
 open class AdevspoonException(
     errorCode: AdevspoonErrorCode,
+    detailReason: String? = null
 ) : RuntimeException() {
-    val errorInfo = errorCode.getErrorInfo()
+    val errorInfo = ErrorInfo(
+        status = errorCode.getErrorInfo().status,
+        code = errorCode.getErrorInfo().code,
+        message = detailReason ?: errorCode.getErrorInfo().message
+    )
 }
