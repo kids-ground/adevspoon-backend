f67788ba66f1ffd96cb458132fe049a7bc7770d2
RokwonK <wonrok97@naver.com>
2024-03-26 21:24:50 +0900

    [feat/#11] ✨ Entity 관련 Legacy Enum 관리용 Converter 구현

--- adevspoon-domain/src/main/kotlin/com/adevspoon/domain/config/converter/LegacyEntityEnumConverter.kt
+++ adevspoon-domain/src/main/kotlin/com/adevspoon/domain/config/converter/LegacyEntityEnumConverter.kt
@@ -1,27 +1,37 @@
 package com.adevspoon.domain.config.converter
 
 import com.adevspoon.domain.common.entity.LegacyEntityEnum
+import com.adevspoon.domain.common.exception.DomainInvalidAttributeException
 import jakarta.persistence.AttributeConverter
-import jakarta.persistence.Converter
 
 /**
  * 각 Enum에서 Converter 구현하기
  * -
  */
 
-@Converter
-class LegacyEntityEnumConverter<T>(
+
+abstract class LegacyEntityEnumConverter<T>(
     private var enumType: Class<T>,
     private var nullable: Boolean = false
 ) : AttributeConverter<T?, String?> where T: Enum<T>, T:LegacyEntityEnum {
     override fun convertToDatabaseColumn(attribute: T?): String? {
         if (!nullable && attribute == null) {
-            throw IllegalArgumentException("Cannot convert null value to database column when nullable is false")
+            throw DomainInvalidAttributeException(type = enumType.simpleName)
         }
-        TODO("Not yet implemented")
+
+        return attribute?.name?.lowercase()
     }
 
     override fun convertToEntityAttribute(dbData: String?): T? {
-        TODO("Not yet implemented")
+        if (!nullable && dbData == null) {
+            throw DomainInvalidAttributeException(type = enumType.simpleName)
+        } else if (nullable && dbData == null) {
+            return null
+        }
+
+        val data = dbData?.uppercase()
+        return enumType.enumConstants
+            .firstOrNull { it.name == data }
+            ?: throw DomainInvalidAttributeException(type = enumType.simpleName)
     }
 }
