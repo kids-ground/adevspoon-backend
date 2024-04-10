78cfa7ff99d0540ee4021ddd18e9b2782f9ff461
Jin-hyuck <92037996+JinDDung2@users.noreply.github.com>
2024-04-10 16:28:47 +0900

    🎨 PostDomainErrorCode 수정

--- adevspoon-domain/src/main/kotlin/com/adevspoon/domain/board/exception/PostDomainErrorCode.kt
+++ adevspoon-domain/src/main/kotlin/com/adevspoon/domain/board/exception/PostDomainErrorCode.kt
@@ -1,19 +1,11 @@
 package com.adevspoon.domain.board.exception
 
-import com.adevspoon.common.dto.ErrorInfo
 import com.adevspoon.common.exception.AdevspoonErrorCode
 
 enum class PostDomainErrorCode(
-    private val status: Int,
-    private val code: Int,
-    private val message: String,
+    override val status: Int,
+    override val code: Int,
+    override val message: String,
 ): AdevspoonErrorCode {
     BOARD_TAG_NOT_FOUND(404, 404_001_000, "등록되지 않은 태그입니다.");
-
-
-    override fun getErrorInfo() = ErrorInfo(
-        status = status,
-        code = code,
-        message = message
-    )
 }
