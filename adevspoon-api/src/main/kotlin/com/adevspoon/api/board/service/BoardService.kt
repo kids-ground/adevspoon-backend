2291adb66d1f2c4c0a606bd0479f9995c52191cc
Jin-hyuck <92037996+JinDDung2@users.noreply.github.com>
2024-04-14 12:07:05 +0900

    🎨 BoardInfoResponse의 from 메소드를 수정하여 각 응답 DTO에서 직접 변환하도록 변경

--- adevspoon-api/src/main/kotlin/com/adevspoon/api/board/service/BoardService.kt
+++ adevspoon-api/src/main/kotlin/com/adevspoon/api/board/service/BoardService.kt
@@ -2,9 +2,7 @@ package com.adevspoon.api.board.service
 
 import com.adevspoon.api.board.dto.request.RegisterBoardPostRequest
 import com.adevspoon.api.board.dto.response.BoardInfoResponse
-import com.adevspoon.api.board.dto.response.BoardTagResponse
 import com.adevspoon.api.common.annotation.ApplicationService
-import com.adevspoon.api.member.dto.response.MemberProfileResponse
 import com.adevspoon.domain.board.service.BoardPostDomainService
 
 @ApplicationService
@@ -13,16 +11,12 @@ class BoardService(
 ) {
     fun registerBoardPost(userId: Long, request: RegisterBoardPostRequest): BoardInfoResponse {
         val boardPost = boardPostDomainService.registerBoardPost(userId, request.tagId, request.title, request.content)
-        val boardTag = BoardTagResponse.from(boardPost.tag)
-        val memberProfileResponse = MemberProfileResponse.from(boardPost.user)
-        return BoardInfoResponse.from(boardPost, boardTag, memberProfileResponse)
+        return BoardInfoResponse.from(boardPost)
     }
 
     fun getBoardPost(userId: Long, postId: Long): BoardInfoResponse {
         val boardPost = boardPostDomainService.getBoardPost(userId, postId)
-        val boardTag = BoardTagResponse.from(boardPost.tag)
-        val memberProfileResponse = MemberProfileResponse.from(boardPost.user)
-        return BoardInfoResponse.from(boardPost, boardTag, memberProfileResponse)
+        return BoardInfoResponse.from(boardPost)
     }
 
 }
