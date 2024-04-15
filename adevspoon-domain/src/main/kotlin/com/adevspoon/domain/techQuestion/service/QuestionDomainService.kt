058d64ace0807bbdb64767299f91d8cd0cdc9c6b
RokwonK <wonrok97@naver.com>
2024-04-15 22:13:12 +0900

    ✨ Question 가져오기 도메인 로직 구현

--- adevspoon-domain/src/main/kotlin/com/adevspoon/domain/techQuestion/service/QuestionDomainService.kt
+++ adevspoon-domain/src/main/kotlin/com/adevspoon/domain/techQuestion/service/QuestionDomainService.kt
@@ -2,19 +2,54 @@ package com.adevspoon.domain.techQuestion.service
 
 import com.adevspoon.domain.common.annotation.DomainService
 import com.adevspoon.domain.member.domain.UserEntity
+import com.adevspoon.domain.member.exception.MemberNotFoundException
+import com.adevspoon.domain.member.repository.UserRepository
+import com.adevspoon.domain.techQuestion.domain.QuestionOpenEntity
 import com.adevspoon.domain.techQuestion.domain.UserCustomizedQuestionCategoryEntity
 import com.adevspoon.domain.techQuestion.domain.UserCustomizedQuestionCategoryId
+import com.adevspoon.domain.techQuestion.dto.response.QuestionInfo
 import com.adevspoon.domain.techQuestion.exception.QuestionCategoryNotFoundException
-import com.adevspoon.domain.techQuestion.exception.QuestionDomainErrorCode
+import com.adevspoon.domain.techQuestion.exception.QuestionNotFoundException
+import com.adevspoon.domain.techQuestion.exception.QuestionNotOpenedException
 import com.adevspoon.domain.techQuestion.repository.QuestionCategoryRepository
+import com.adevspoon.domain.techQuestion.repository.QuestionOpenRepository
+import com.adevspoon.domain.techQuestion.repository.QuestionRepository
 import com.adevspoon.domain.techQuestion.repository.UserCustomizedQuestionCategoryRepository
+import org.slf4j.LoggerFactory
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.transaction.annotation.Transactional
+import java.time.LocalDate
 
 @DomainService
 class QuestionDomainService(
     private val questionCategoryRepository: QuestionCategoryRepository,
-    private val userCustomizedQuestionCategoryRepository: UserCustomizedQuestionCategoryRepository
+    private val questionRepository: QuestionRepository,
+    private val questionOpenRepository: QuestionOpenRepository,
+    private val userRepository: UserRepository,
+    private val userCustomizedQuestionCategoryRepository: UserCustomizedQuestionCategoryRepository,
+    private val questionOpenDomainService: QuestionOpenDomainService,
 ) {
+    private val logger = LoggerFactory.getLogger(this.javaClass)!!
+    @Transactional(readOnly = true)
+    fun getQuestion(memberId: Long, questionId: Long): QuestionInfo {
+        val user = userRepository.findByIdOrNull(memberId) ?: throw throw MemberNotFoundException()
+        val question = questionRepository.findByIdOrNull(questionId) ?: throw throw QuestionNotFoundException()
+        val issuedQuestion = questionOpenRepository.findByQuestionAndUser(question, user) ?: throw throw QuestionNotOpenedException()
+
+        return makeQuestionInfo(issuedQuestion)
+    }
+
+    // TODO: Distributed Lock 필요 (트랜잭션 전에 GetLock, 트랜잭션 후에 ReleaseLock)
+    @Transactional
+    fun getOrCreateTodayQuestion(memberId: Long, today: LocalDate): QuestionInfo {
+        val user = userRepository.findByIdOrNull(memberId) ?: throw throw MemberNotFoundException()
+        val latestIssuedQuestion = questionOpenRepository.findLatest(user)
+
+        val isTodayQuestion = (latestIssuedQuestion?.openDate?.toLocalDate()?.compareTo(today) ?: -1) == 0
+
+        return if(isTodayQuestion) makeQuestionInfo(latestIssuedQuestion!!)
+        else questionOpenDomainService.issueQuestion(memberId, today)
+    }
 
     @Transactional
     fun updateQuestionCategory(user: UserEntity, categoryIds: List<Long>) {
@@ -26,8 +61,14 @@ class QuestionDomainService(
             ?.map { UserCustomizedQuestionCategoryEntity(UserCustomizedQuestionCategoryId(it, user)) }
             ?: throw QuestionCategoryNotFoundException()
 
-        userCustomizedQuestionCategoryRepository.deleteAllByUserId(user.id)
-        // TODO: saveAll 한 번에 처리 필요(select N번, insert N번 나감)
+        userCustomizedQuestionCategoryRepository.deleteAllByUser(user)
+        // FIXME: saveAll 한 번에 처리 필요(select N번, insert N번 나감)
         userCustomizedQuestionCategoryRepository.saveAll(newCustomizedQuestionCategories)
+    }
+
+    private fun makeQuestionInfo(questionOpen: QuestionOpenEntity): QuestionInfo {
+        val category = questionCategoryRepository.findByIdOrNull(questionOpen.question.categoryId)
+            ?: throw QuestionCategoryNotFoundException()
+        return QuestionInfo.from(questionOpen, category.category)
     }
 }
