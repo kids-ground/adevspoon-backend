aa47f73731bad479c75c81d73794c931f503d1f9
RokwonK <wonrok97@naver.com>
2024-04-15 22:14:07 +0900

    ✨ Question GET API 구현

--- adevspoon-api/src/main/kotlin/com/adevspoon/api/question/dto/request/QuestionListRequest.kt
+++ adevspoon-api/src/main/kotlin/com/adevspoon/api/question/dto/request/QuestionListRequest.kt
@@ -3,7 +3,6 @@ package com.adevspoon.api.question.dto.request
 import io.swagger.v3.oas.annotations.media.Schema
 
 
-// TODO: Enum Validate, Converter 필요
 data class QuestionListRequest(
     val sort: QuestionSortType = QuestionSortType.NEWEST,
     @Schema(description = "등록한 답변만 가져올것인지 여부")
