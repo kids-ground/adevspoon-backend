3334df34a810e22ee2acc63ad72b101798c1b6cb
RokwonK <wonrok97@naver.com>
2024-04-16 22:52:28 +0900

    ✨ Domain 내 Lock 관련 예외 추가

--- adevspoon-domain/src/main/kotlin/com/adevspoon/domain/common/exception/DomainCommonError.kt
+++ adevspoon-domain/src/main/kotlin/com/adevspoon/domain/common/exception/DomainCommonError.kt
@@ -8,5 +8,7 @@ enum class DomainCommonError(
     override val error: DomainType.Error,
     override val message: String,
 ): AdevspoonErrorCode {
-    INVALID_ATTRIBUTE(DomainType.COMMON code 500 no 1, "유효하지 않은 값 사용");
+    INVALID_ATTRIBUTE(DomainType.COMMON code 500 no 1, "유효하지 않은 값 사용"),
+
+    FAIL_TO_GET_LOCK(DomainType.COMMON code 500 no 2, "서버 내부 오류입니다. 관리자에게 문의하세요");
 }
--- adevspoon-domain/src/main/kotlin/com/adevspoon/domain/common/exception/DomainException.kt
+++ adevspoon-domain/src/main/kotlin/com/adevspoon/domain/common/exception/DomainException.kt
@@ -3,3 +3,4 @@ package com.adevspoon.domain.common.exception
 import com.adevspoon.common.exception.AdevspoonException
 
 class DomainInvalidAttributeException(type: String? = null): AdevspoonException(DomainCommonError.INVALID_ATTRIBUTE, type?.let { "$it 타입 필드에 유효하지 않은 값이 들어갔습니다" })
+class DomainFailToGetLockException: AdevspoonException(DomainCommonError.FAIL_TO_GET_LOCK)
