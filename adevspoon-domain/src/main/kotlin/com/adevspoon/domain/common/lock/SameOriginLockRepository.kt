7056c8e85efc94650337f9bdd31677f8b0f44d40
RokwonK <wonrok97@naver.com>
2024-04-18 01:19:29 +0900

    ✨ SameOriginLockRepository 구현 완료

--- adevspoon-domain/src/main/kotlin/com/adevspoon/domain/common/lock/SameOriginLockRepository.kt
+++ adevspoon-domain/src/main/kotlin/com/adevspoon/domain/common/lock/SameOriginLockRepository.kt
@@ -1,17 +1,89 @@
 package com.adevspoon.domain.common.lock
 
-import org.springframework.stereotype.Component
-import javax.sql.DataSource
+import com.adevspoon.domain.common.annotation.LockDataSource
+import com.adevspoon.domain.common.exception.DomainFailToGetLockException
+import com.adevspoon.domain.common.exception.DomainFailToReleaseLockException
+import com.adevspoon.domain.common.lock.interfaces.DistributedLockRepository
+import org.slf4j.LoggerFactory
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
 
-@Component
+private const val GET_LOCK = "SELECT GET_LOCK(:key, :timeout)"
+private const val RELEASE_LOCK = "SELECT RELEASE_LOCK(:key)"
+
+
 class SameOriginLockRepository(
-    private val datasource: DataSource
+    @LockDataSource private val jdbcTemplate: NamedParameterJdbcTemplate
 ): DistributedLockRepository {
-    override fun getLock(lockKey: String, timeout: Int, leaseTime: Int): Boolean {
-        TODO("Not yet implemented")
+    private val logger = LoggerFactory.getLogger(this.javaClass)!!
+
+    override fun <R> withLock(key: LockKey, block: () -> R?): R? {
+        try {
+            getLock(key)
+            return block()
+        } finally {
+            releaseLock(key)
+        }
     }
 
-    override fun releaseLock(lockKey: String): Boolean {
-        TODO("Not yet implemented")
+    override fun <R> withAllLock(keys: List<LockKey>, block: () -> R?): R? {
+        try {
+            getAllLock(keys)
+            return block()
+        } finally {
+            releaseAllLock(keys)
+        }
+    }
+
+    override fun getAllLock(keys: List<LockKey>): Boolean {
+        keys.map { getLock(it) }
+        return true
+    }
+
+    override fun releaseAllLock(keys: List<LockKey>): Boolean {
+        keys.map { releaseLock(it) }
+        return true
+    }
+
+    override fun getLock(key: LockKey): Boolean {
+        val params: MutableMap<String, Any?> = mutableMapOf( "key" to key.name, "timeout" to key.timeout)
+        val result = jdbcTemplate.queryForObject(GET_LOCK, params, Int::class.java)
+        return checkResult(result, key.name, LockType.GET)
+    }
+
+    override fun releaseLock(key: LockKey): Boolean {
+        val params: MutableMap<String, Any?> = mutableMapOf( "key" to key.name)
+        val result = jdbcTemplate.queryForObject(RELEASE_LOCK, params, Int::class.java)
+        return checkResult(result, key.name, LockType.RELEASE)
+    }
+
+    private fun checkResult(
+        result: Int?,
+        key: String,
+        type: LockType
+    ): Boolean {
+        when(result) {
+            1 -> logger.info("Lock ${type.name} 성공 : $key")
+            null -> {
+                if (type == LockType.GET) {
+                    logger.error("Lock ${type.name} 실패 : $key")
+                    throw DomainFailToGetLockException(key, "에러 발생")
+                }
+            }
+            else -> {
+                if (type == LockType.GET) {
+                    logger.error("Lock ${type.name} Timeout : $key, result : $result")
+                    throw DomainFailToGetLockException(key, "타임아웃")
+                } else {
+                    logger.error("Lock ${type.name} Not OWNER : $key, result : $result")
+                    throw DomainFailToReleaseLockException(key, "Key의 주인이 아님")
+                }
+            }
+        }
+
+        return true
+    }
+
+    enum class LockType {
+        GET, RELEASE
     }
 }
