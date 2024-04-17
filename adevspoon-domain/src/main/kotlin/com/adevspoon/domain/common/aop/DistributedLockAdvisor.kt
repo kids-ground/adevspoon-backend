548b52950ac423c565a42be53593cdbd2a2e2fa9
RokwonK <wonrok97@naver.com>
2024-04-18 01:18:52 +0900

    ✨ DistributedLockAdvisor 구현

--- adevspoon-domain/src/main/kotlin/com/adevspoon/domain/common/aop/DistributedLockAdvisor.kt
+++ adevspoon-domain/src/main/kotlin/com/adevspoon/domain/common/aop/DistributedLockAdvisor.kt
@@ -1,46 +1,41 @@
 package com.adevspoon.domain.common.aop
 
 import com.adevspoon.domain.common.annotation.DistributedLock
-import com.adevspoon.domain.common.exception.DomainFailToGetLockException
-import com.adevspoon.domain.common.lock.DistributedLockKey
-import com.adevspoon.domain.common.lock.DistributedLockRepository
+import com.adevspoon.domain.common.exception.DomainLockKeyNotSetException
+import com.adevspoon.domain.common.lock.LockKey
+import com.adevspoon.domain.common.lock.interfaces.DistributedLockRepository
 import org.aspectj.lang.ProceedingJoinPoint
 import org.aspectj.lang.annotation.Around
 import org.aspectj.lang.annotation.Aspect
 import org.aspectj.lang.reflect.MethodSignature
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
+import org.springframework.core.annotation.Order
 import org.springframework.stereotype.Component
 import kotlin.reflect.KClass
 
 @Aspect
 @Component
+@Order(1)
 class DistributedLockAdvisor(
     private val lockRepository: DistributedLockRepository
 ) {
-    val logger: Logger = LoggerFactory.getLogger(this.javaClass)
-
     @Around("@annotation(com.adevspoon.domain.common.annotation.DistributedLock)")
     fun atTarget(joinPoint: ProceedingJoinPoint): Any? {
-        val lockAnnotation = (joinPoint.signature as MethodSignature).method
-            .getAnnotation(DistributedLock::class.java)
+        val lockAnnotation = getAnnotation(joinPoint)
         val lockKeyList = getLockKeyList(joinPoint.args, lockAnnotation.keyClass)
 
-        try {
-            lockKeyList.map { lockRepository.getLock(it.lockKey, it.timeout, it.leaseTime) }
-                .filter { !it }
-                .takeIf { it.isEmpty() }
-                ?: throw DomainFailToGetLockException()
-            logger.info("DistributedLock Get : ${lockKeyList.joinToString(", ") { it.lockKey }}")
-            return joinPoint.proceed()
-        } finally {
-            logger.info("DistributedLock Release : ${lockKeyList.joinToString(", ") { it.lockKey }}")
-            lockKeyList.forEach { lockRepository.releaseLock(it.lockKey) }
+        return lockRepository.withAllLock(lockKeyList) {
+            joinPoint.proceed()
         }
     }
 
-    private fun getLockKeyList(arguments: Array<Any>, keyClasses: Array<KClass<out DistributedLockKey>>) =
-        arguments.filterIsInstance<DistributedLockKey>()
+    private fun getAnnotation(joinPoint: ProceedingJoinPoint) =
+        (joinPoint.signature as MethodSignature).method
+            .getAnnotation(DistributedLock::class.java)
+
+    private fun getLockKeyList(arguments: Array<Any>, keyClasses: Array<KClass<out LockKey>>) =
+        arguments.filterIsInstance<LockKey>()
             .filter { keyClasses.isEmpty() || keyClasses.contains(it::class) }
             .toList()
+            .takeIf { it.isNotEmpty() }
+            ?: throw DomainLockKeyNotSetException()
 }
