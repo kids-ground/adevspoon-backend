0c6876978b6133f0efd3979bb011ec66a0d44e82
RokwonK <wonrok97@naver.com>
2024-04-19 21:18:04 +0900

    ✨ AOP 우선순위 조절을 위한 트랜잭션 AOP 우선순위 조절

--- adevspoon-domain/src/main/kotlin/com/adevspoon/domain/config/JpaConfig.kt
+++ adevspoon-domain/src/main/kotlin/com/adevspoon/domain/config/JpaConfig.kt
@@ -7,8 +7,10 @@ import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Primary
+import org.springframework.core.Ordered
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+import org.springframework.transaction.annotation.EnableTransactionManagement
 import javax.sql.DataSource
 
 
@@ -16,6 +18,8 @@ import javax.sql.DataSource
 @EnableJpaAuditing
 @EntityScan(basePackages = ["com.adevspoon.domain"])
 @EnableJpaRepositories(basePackages = ["com.adevspoon.domain"])
+// Transaction 전 후로 실행되는 AOP를 위해 직접 설정
+@EnableTransactionManagement(order = Ordered.LOWEST_PRECEDENCE - 10)
 class JpaConfig {
 
     @Bean
