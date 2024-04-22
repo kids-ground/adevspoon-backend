2bfc46b1330c9aa3324d2b1100fa33af92e6577f
RokwonK <wonrok97@naver.com>
2024-04-22 16:22:09 +0900

    ✨ Category별 Question 수 가져오는 쿼리 추가

--- adevspoon-domain/src/main/kotlin/com/adevspoon/domain/techQuestion/repository/QuestionRepository.kt
+++ adevspoon-domain/src/main/kotlin/com/adevspoon/domain/techQuestion/repository/QuestionRepository.kt
@@ -1,10 +1,16 @@
 package com.adevspoon.domain.techQuestion.repository
 
 import com.adevspoon.domain.techQuestion.domain.QuestionEntity
+import com.adevspoon.domain.techQuestion.dto.response.CategoryQuestionCountDto
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 
 interface QuestionRepository: JpaRepository<QuestionEntity, Long> {
     @Query("SELECT q.id from QuestionEntity q where q.categoryId in :categoryIds")
     fun findAllQuestionIds(categoryIds: List<Long>): Set<Long>
+
+    @Query("SELECT new com.adevspoon.domain.techQuestion.dto.response.CategoryQuestionCountDto(q.categoryId, COUNT(q)) " +
+            "FROM QuestionEntity q " +
+            "GROUP BY q.categoryId")
+    fun findQuestionCountGroupByCategory(): List<CategoryQuestionCountDto>
 }
