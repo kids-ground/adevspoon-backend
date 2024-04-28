e4e248f1a2124ba15e7f449e5fb7d5609d5960b9
RokwonK <wonrok97@naver.com>
2024-04-28 20:21:47 +0900

    ✨ Async AOP 트랜잭션 AOP보다 높은 우선순위 부여

--- adevspoon-domain/src/main/kotlin/com/adevspoon/domain/config/AsyncConfig.kt
+++ adevspoon-domain/src/main/kotlin/com/adevspoon/domain/config/AsyncConfig.kt
@@ -1,8 +1,9 @@
 package com.adevspoon.domain.config
 
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.Ordered
 import org.springframework.scheduling.annotation.EnableAsync
 
 @Configuration
-@EnableAsync
+@EnableAsync(order = Ordered.LOWEST_PRECEDENCE - 11) // 트랜잭션 AOP보다 먼저 처리
 class AsyncConfig
