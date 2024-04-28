1b78034bfcd3c97adab25eaa4c50627405e60c46
RokwonK <wonrok97@naver.com>
2024-04-28 15:40:48 +0900

    ✨ BoardPost, Answer 등록시 잘못된 응답타입일때의 예외 추가

--- adevspoon-domain/src/main/kotlin/com/adevspoon/domain/board/exception/PostDomainException.kt
+++ adevspoon-domain/src/main/kotlin/com/adevspoon/domain/board/exception/PostDomainException.kt
@@ -12,3 +12,4 @@ class BoardPostNotFoundException(postId: String): AdevspoonException(
 class BoardPostOwnershipException(postOwnerId: String, loginUserId: String): AdevspoonException(
     BOARD_POST_EDIT_UNAUTHORIZED,
     detailReason = BOARD_POST_EDIT_UNAUTHORIZED.message + " postOwnerId: $postOwnerId, loginUserId: $loginUserId")
+class BoardPostInvalidReturnException: AdevspoonException(BOARD_POST_INVALID_RETURN)
