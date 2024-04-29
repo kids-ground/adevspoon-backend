a82b708de432fc7f15f08e7a77534d0f6c158a9f
RokwonK <wonrok97@naver.com>
2024-04-29 14:28:46 +0900

    ✨ Answer 가져오는 쿼리 추가

--- adevspoon-domain/src/main/kotlin/com/adevspoon/domain/techQuestion/repository/AnswerRepository.kt
+++ adevspoon-domain/src/main/kotlin/com/adevspoon/domain/techQuestion/repository/AnswerRepository.kt
@@ -2,7 +2,13 @@ package com.adevspoon.domain.techQuestion.repository
 
 import com.adevspoon.domain.techQuestion.domain.AnswerEntity
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 
 interface AnswerRepository: JpaRepository<AnswerEntity, Long> {
 
+    @Query("SELECT a FROM AnswerEntity a " +
+            "LEFT JOIN FETCH a.question " +
+            "LEFT JOIN FETCH a.user " +
+            "WHERE a.id = :answerId")
+    fun findWithQuestionAndUser(answerId: Long): AnswerEntity?
 }
