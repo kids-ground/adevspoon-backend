c934eab5ef72d79404d557b373f4d8a060d50f52
Jin-hyuck <92037996+JinDDung2@users.noreply.github.com>
2024-04-29 15:34:51 +0900

    :art: postType, user, reason non-null하게 변경 및 isRead 디폴트값 false로 변경

--- adevspoon-domain/src/main/kotlin/com/adevspoon/domain/common/entity/ReportEntity.kt
+++ adevspoon-domain/src/main/kotlin/com/adevspoon/domain/common/entity/ReportEntity.kt
@@ -1,8 +1,8 @@
 package com.adevspoon.domain.common.entity
 
 import com.adevspoon.domain.common.entity.enums.ReportReason
-import com.adevspoon.domain.techQuestion.domain.AnswerEntity
 import com.adevspoon.domain.member.domain.UserEntity
+import com.adevspoon.domain.techQuestion.domain.AnswerEntity
 import jakarta.persistence.*
 import jakarta.validation.constraints.NotNull
 import jakarta.validation.constraints.Size
@@ -20,18 +20,18 @@ class ReportEntity(
     @Size(max = 50)
     @NotNull
     @Column(name = "post_type", nullable = false, length = 50)
-    val postType: String? = null,
+    val postType: String,
 
     @ManyToOne(fetch = FetchType.LAZY)
     @OnDelete(action = OnDeleteAction.SET_NULL)
     @JoinColumn(name = "user_id")
-    val user: UserEntity? = null,
+    val user: UserEntity,
 
     @Column(name = "reason", columnDefinition = "ENUM('abuse','spammer','obscene','scam', 'political_agitation', 'illegal_ads_and_sales', 'etc')")
-    val reason: ReportReason? = null,
+    val reason: ReportReason,
 
     @Column(name = "is_read")
-    var isRead: Boolean? = null,
+    var isRead: Boolean? = false,
 
     @ManyToOne(fetch = FetchType.LAZY)
     @OnDelete(action = OnDeleteAction.CASCADE)
