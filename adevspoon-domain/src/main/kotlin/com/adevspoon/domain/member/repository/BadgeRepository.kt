b1c1871611df7850b24daff3e761f90a145d2c21
RokwonK <wonrok97@naver.com>
2024-04-29 14:28:31 +0900

    🎨 BadgeRepository 잘못된 id 타입 수정

--- adevspoon-domain/src/main/kotlin/com/adevspoon/domain/member/repository/BadgeRepository.kt
+++ adevspoon-domain/src/main/kotlin/com/adevspoon/domain/member/repository/BadgeRepository.kt
@@ -3,5 +3,5 @@ package com.adevspoon.domain.member.repository
 import com.adevspoon.domain.member.domain.BadgeEntity
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface BadgeRepository: JpaRepository<BadgeEntity, Long> {
+interface BadgeRepository: JpaRepository<BadgeEntity, Int> {
 }
