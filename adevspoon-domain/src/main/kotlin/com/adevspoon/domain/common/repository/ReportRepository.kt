02471af87194526df49aed118cbd9f2487e9a381
Jin-hyuck <92037996+JinDDung2@users.noreply.github.com>
2024-04-29 17:28:35 +0900

    :sparkles: 해당 신고 존재 유무 쿼리 추가

--- adevspoon-domain/src/main/kotlin/com/adevspoon/domain/common/repository/ReportRepository.kt
+++ adevspoon-domain/src/main/kotlin/com/adevspoon/domain/common/repository/ReportRepository.kt
@@ -2,5 +2,12 @@ package com.adevspoon.domain.common.repository
 
 import com.adevspoon.domain.common.entity.ReportEntity
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 
-interface ReportRepository : JpaRepository<ReportEntity, Long>
+interface ReportRepository : JpaRepository<ReportEntity, Long> {
+    @Query("SELECT CASE WHEN count (r) > 0 THEN TRUE ELSE FALSE END FROM ReportEntity r " +
+        "WHERE (r.postType = :postType " +
+        "AND (r.postType = 'board_post' AND r.boardPostId = :contentId) " +
+        "OR (r.postType = 'board_comment' AND  r.boardCommentId =: contentId))")
+    fun existsByPostTypeAndContentId(postType: String, contentId: Long): Boolean
+}
