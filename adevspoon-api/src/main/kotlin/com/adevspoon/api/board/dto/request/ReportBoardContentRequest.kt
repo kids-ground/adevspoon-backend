eb3299b9a3c3418dc57ff5abdf36a438792500c6
Jin-hyuck <92037996+JinDDung2@users.noreply.github.com>
2024-04-29 18:14:42 +0900

    Feat/#52. 신고하기 API (#55)

--- adevspoon-api/src/main/kotlin/com/adevspoon/api/board/dto/request/ReportBoardContentRequest.kt
+++ adevspoon-api/src/main/kotlin/com/adevspoon/api/board/dto/request/ReportBoardContentRequest.kt
@@ -1,16 +1,27 @@
 package com.adevspoon.api.board.dto.request
 
 import com.adevspoon.api.answer.dto.request.PostReportType
+import com.adevspoon.domain.board.dto.request.CreateReportRequest
 import io.swagger.v3.oas.annotations.media.Schema
-import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Min
+import jakarta.validation.constraints.NotNull
 
 // TODO: Enum validation 필요
 data class ReportBoardContentRequest(
     @Schema(description = "게시글 or 댓글", example = "board_post", required = true,)
     val type: BoardContentType,
     @Schema(description = "타입에 따른 postId", required = true)
-    @field:NotBlank(message = "게시글 or 댓글 id는 필수입니다.")
+    @field:NotNull(message = "게시글 or 댓글 id는 필수입니다.")
+    @field:Min(1, message = "게시글 or 댓글 id는 0보다 커야합니다.")
     val contentId: Long,
     @Schema(description = "신고 이유", example = "none", nullable = true, defaultValue = "none")
     val reason: PostReportType = PostReportType.NONE,
-)
+) {
+    fun toCreateReportRequest() = CreateReportRequest(
+        type = when (type) {
+            BoardContentType.BOARD_POST -> BoardContentType.BOARD_POST.toString()
+            BoardContentType.BOARD_COMMENT -> BoardContentType.BOARD_COMMENT.toString()
+        },
+        contentId = this.contentId,
+    )
+}
---
+++ adevspoon-domain/src/main/kotlin/com/adevspoon/domain/board/dto/request/CreateReportRequest.kt
@@ -0,0 +1,6 @@
+package com.adevspoon.domain.board.dto.request
+
+data class CreateReportRequest(
+    val type: Any,
+    val contentId: Long,
+)
