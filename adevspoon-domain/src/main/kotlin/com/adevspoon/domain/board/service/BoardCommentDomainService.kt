8fe1709e11f6b52d1c0d4ade7043f9752ba257a3
Jin-hyuck <92037996+JinDDung2@users.noreply.github.com>
2024-04-30 21:48:55 +0900

    :bug: user의 badge 가져오는 N + 1 미해결. 이전 상태로 복구

--- adevspoon-domain/src/main/kotlin/com/adevspoon/domain/board/service/BoardCommentDomainService.kt
+++ adevspoon-domain/src/main/kotlin/com/adevspoon/domain/board/service/BoardCommentDomainService.kt
@@ -8,7 +8,6 @@ import com.adevspoon.domain.board.repository.BoardCommentRepository
 import com.adevspoon.domain.board.repository.BoardPostRepository
 import com.adevspoon.domain.common.annotation.DomainService
 import com.adevspoon.domain.common.service.LikeDomainService
-import com.adevspoon.domain.member.repository.BadgeRepository
 import com.adevspoon.domain.member.service.MemberDomainService
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.transaction.annotation.Transactional
@@ -17,7 +16,6 @@ import org.springframework.transaction.annotation.Transactional
 class BoardCommentDomainService(
     private val boardCommentRepository: BoardCommentRepository,
     private val boardPostRepository: BoardPostRepository,
-    private val badgeRepository: BadgeRepository,
     private val memberDomainService: MemberDomainService,
     private val likeDomainService: LikeDomainService
 ) {
@@ -27,11 +25,10 @@ class BoardCommentDomainService(
         val commentAndAuthors = boardCommentRepository.findCommentAuthorsByPost(boardPost)
         val commentIds = commentAndAuthors.map { it.comment.id }
         val likedCommentIds = getLikedCommentsByUser(userId, commentIds)
-        val badges = badgeRepository.findAll()
         return commentAndAuthors.map { commentAndAuthor ->
             BoardComment.from(
                 comment = commentAndAuthor.comment,
-                memberProfile = memberDomainService.getOtherMemberProfile(commentAndAuthor.user.id, badges),
+                memberProfile = memberDomainService.getOtherMemberProfile(commentAndAuthor.user.id),
                 isLike = likedCommentIds.contains(commentAndAuthor.comment.id),
                 isMine = commentAndAuthor.user.id == userId
             )
