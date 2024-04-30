a0a5728ff225d3c9f0d279e8cb5c74e6b0d3e50b
Jin-hyuck <92037996+JinDDung2@users.noreply.github.com>
2024-04-30 18:45:28 +0900

    :sparkles: 게시글에 해당하는 댓글 response

--- adevspoon-api/src/main/kotlin/com/adevspoon/api/board/dto/response/BoardCommentListResponse.kt
+++ adevspoon-api/src/main/kotlin/com/adevspoon/api/board/dto/response/BoardCommentListResponse.kt
@@ -1,5 +1,28 @@
 package com.adevspoon.api.board.dto.response
 
+import com.adevspoon.api.member.dto.response.MemberProfileResponse
+import com.adevspoon.domain.board.dto.response.BoardComment
+
 data class BoardCommentListResponse(
     val list: List<BoardCommentResponse>,
-)
+) {
+    companion object {
+        fun from(commentList: List<BoardComment>) : BoardCommentListResponse{
+            val commentResponseList = commentList.map { comment ->
+                BoardCommentResponse(
+                id = comment.id,
+                postId = comment.postId,
+                content = comment.content,
+                user = MemberProfileResponse.from(comment.user),
+                isLiked = comment.isLiked,
+                isMine = comment.isMine,
+                likeCount = comment.likeCount,
+                createdAt = comment.createdAt,
+                updatedAt = comment.updatedAt
+                )
+            }
+
+            return BoardCommentListResponse(commentResponseList)
+        }
+    }
+}
