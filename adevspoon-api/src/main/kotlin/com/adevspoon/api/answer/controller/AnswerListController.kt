54e7b30013fdf379dac2d87a77bb963efa1503c5
RokwonK <wonrok97@naver.com>
2024-05-02 00:50:35 +0900

    ✨ AnswerList API 구현

--- adevspoon-api/src/main/kotlin/com/adevspoon/api/answer/controller/AnswerListController.kt
+++ adevspoon-api/src/main/kotlin/com/adevspoon/api/answer/controller/AnswerListController.kt
@@ -2,6 +2,7 @@ package com.adevspoon.api.answer.controller
 
 import com.adevspoon.api.answer.dto.request.AnswerListQueryRequest
 import com.adevspoon.api.answer.dto.response.AnswerListResponse
+import com.adevspoon.api.answer.service.AnswerService
 import com.adevspoon.api.common.annotation.RequestUser
 import com.adevspoon.api.common.dto.RequestUserInfo
 import com.adevspoon.api.config.swagger.SWAGGER_TAG_QUESTION_ANSWER
@@ -15,17 +16,15 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/postList")
 @Tag(name = SWAGGER_TAG_QUESTION_ANSWER)
-class AnswerListController {
-
+class AnswerListController(
+    private val answerService: AnswerService
+) {
     @Operation(summary = "특정 질문에 대한 답변 리스트", description = "질문 id에 해당하는 답변 리스트를 가져온다.")
     @GetMapping
     fun getAnswerList(
         @RequestUser user: RequestUserInfo,
         @Valid request: AnswerListQueryRequest
     ): AnswerListResponse {
-        TODO("""
-            - 답변 리스트를 가져온다.
-            - sort가 best인경우 현재유저가 받은 Today 질문의 Best 답변을 가져온다.
-        """.trimIndent())
+        return answerService.getAnswerList(request, user.userId)
     }
 }
