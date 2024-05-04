204880805af5fd1c5364491af12079a65df8b23e
Jin-hyuck <92037996+JinDDung2@users.noreply.github.com>
2024-05-04 16:28:05 +0900

    :bug: tag, take can be nullable

--- adevspoon-domain/src/main/kotlin/com/adevspoon/domain/board/dto/request/GetPostListRequestDto.kt
+++ adevspoon-domain/src/main/kotlin/com/adevspoon/domain/board/dto/request/GetPostListRequestDto.kt
@@ -1,8 +1,8 @@
 package com.adevspoon.domain.board.dto.request
 
 data class GetPostListRequestDto(
-    val tags: List<Int>,
+    val tags: List<Int>?,
     val pageSize: Int = 10,
-    val startPostId: Long? = null,
+    val startPostId: Long?,
     val targetUserId: Long?
 )
