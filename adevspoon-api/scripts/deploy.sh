f2e06d64e29a55e78a67f23a2428db266ae5410b
RokwonK <wonrok97@naver.com>
2024-05-07 15:36:20 +0900

    ✨ 배포자동화 추가

---
+++ adevspoon-api/scripts/deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+ROOT_PATH="/home/ec2-user/adevspoon"
+JAR="$ROOT_PATH/app.jar"
+NOW=$(date +%c)
+
+echo "[$NOW] $JAR 복사"
+cp $ROOT_PATH/build/libs/adevspoon-api-0.0.1-SNAPSHOT.jar $JAR
+
+echo "[$NOW] > $JAR 실행"
+nohup java -jar -Dspring.profiles.active=prod $JAR &
+
+SERVICE_PID=$(pgrep -f $JAR)
+echo "[$NOW] > 서비스 PID: $SERVICE_PID"
