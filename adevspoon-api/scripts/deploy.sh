c86e07fc0977b271374c7f273d648ed791933b4e
RokwonK <wonrok97@naver.com>
2024-05-07 21:07:09 +0900

    🔨 deploy 스크립트 수정

--- adevspoon-api/scripts/deploy.sh
+++ adevspoon-api/scripts/deploy.sh
@@ -8,7 +8,7 @@ echo "[$NOW] $JAR 복사"
 cp $ROOT_PATH/build/libs/adevspoon-api-0.0.1-SNAPSHOT.jar $JAR
 
 echo "[$NOW] > $JAR 실행"
-nohup java -jar -Dspring.profiles.active=prod $JAR $ROOT_PATH/nohup.out 2>&1 &
+nohup java -jar -Dspring.profiles.active=prod $JAR > /dev/null 2> /dev/null < /dev/null &
 
 
 SERVICE_PID=$(pgrep -f $JAR)
