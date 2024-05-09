a6dee45df30a2139ff2e276fa0972a0609e845c5
Jin-hyuck <92037996+JinDDung2@users.noreply.github.com>
2024-05-09 20:02:06 +0900

    :sparkles: slack 의존성 추가

--- adevspoon-domain/build.gradle.kts
+++ adevspoon-domain/build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
 dependencies {
     implementation(project(":adevspoon-common"))
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("com.slack.api:slack-api-client:1.38.0")
     //querydsl
     implementation("com.querydsl:querydsl-jpa:5.0.0:jakarta")
     kapt("com.querydsl:querydsl-apt:5.0.0:jakarta")
