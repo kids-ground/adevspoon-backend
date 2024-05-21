0d07e3297d222cc230598590875f3def4ce168b8
RokwonK <wonrok97@naver.com>
2024-05-21 15:05:14 +0900

    🎨 Configuration 수정

--- adevspoon-infrastructure/src/main/kotlin/com/adevspoon/infrastructure/alarm/dto/AlarmType.kt
+++ adevspoon-infrastructure/src/main/kotlin/com/adevspoon/infrastructure/alarm/dto/AlarmType.kt
@@ -3,6 +3,7 @@ package com.adevspoon.infrastructure.alarm.dto
 enum class AlarmType(
     val title: String,
 ) {
+    CHECK("체크 ✔️"),
     REPORT("신고 접수 ⚠️"),
     BATCH_COMPLETE("Batch 성공 ✅"),
     BATCH_ERROR("Batch 처리 에러 🚨"),
--- adevspoon-infrastructure/src/main/kotlin/com/adevspoon/infrastructure/storage/config/StorageConfig.kt
+++ adevspoon-infrastructure/src/main/kotlin/com/adevspoon/infrastructure/storage/config/StorageConfig.kt
@@ -1,10 +1,12 @@
 package com.adevspoon.infrastructure.storage.config
 
 
+import com.adevspoon.infrastructure.storage.dto.FileInfo
 import com.adevspoon.infrastructure.storage.service.S3StorageAdapter
 import com.adevspoon.infrastructure.storage.service.StorageAdapter
 import com.amazonaws.services.s3.AmazonS3
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -14,7 +16,21 @@ class StorageConfig {
 
     @Bean
     @ConditionalOnMissingBean(StorageAdapter::class)
+    @ConditionalOnProperty(prefix = "cloud.aws", name = ["s3.bucket"])
     fun s3Storage(s3Client: AmazonS3): StorageAdapter {
         return S3StorageAdapter(s3Client)
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(StorageAdapter::class)
+    fun dummyStorage(): StorageAdapter {
+        return object: StorageAdapter {
+            override fun uploadImage(file: FileInfo): String {
+                return ""
+            }
+            override fun uploadImageList(files: List<FileInfo>): List<String> {
+                return listOf("")
+            }
+        }
     }
 }
